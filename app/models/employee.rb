2863c98b789a46a98415a2990e8bb00d451b072e
Bill <billfeng2002@gmail.com>
2021-07-06 12:09:27 -0700

    commit tree dbead26097d5968692ea2502e11439053af6a01a

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,2 +1,9 @@
 class Employee < ApplicationRecord
+    belongs_to :dog
+    validates :title, presence: true, uniqueness: { message: "needs to be unique" }
+    validates :alias, presence: true, uniqueness: { message: "needs to be unique" }
+
+    def to_s
+        self.first_name + " " + self.last_name
+    end
 end
