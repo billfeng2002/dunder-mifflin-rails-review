a59650e9c90e1cacb010fdc350bac7c45639b224
Bill <billfeng2002@gmail.com>
2021-07-06 12:23:55 -0700

    work with margaret done

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,13 @@
 class DogsController < ApplicationController
+    def index
+        @dogs=Dog.all
+    end
+    def show
+        @dog=Dog.find(params[:id])
+    end
+
+    def most_popular
+        @dogs = Dog.all.sort_by{|dog| -dog.employees.count}
+        render :index
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'dogs/most_popular', to: "dogs#most_popular"
   resources :dogs
   resources :employees
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
